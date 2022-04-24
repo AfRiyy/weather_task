3c9fa507132db4080ad48d1acd157ebde91e9c29
AfRiyy <madaraszdavidd@gmail.com>
2022-04-24 22:40:44 +0200

    modellek

---
+++ app/Models/Permission.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $fillable = [
+        "permission"
+    ];
+    public function users()
+    {
+        return $this->hasOneThrough(Permission::class, userpermission::class);
+    }
+}
