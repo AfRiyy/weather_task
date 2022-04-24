f804d6bc56c298ff9db91469e3f885611e99cc2a
AfRiyy <madaraszdavidd@gmail.com>
2022-04-24 22:41:04 +0200

    útvonalak

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PermissionController;
+use App\Models\Permission;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+Route::get('/permissions', [PermissionController::class, "index"]);
+Route::get('/permissions/create', [PermissionController::class, "create"]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
