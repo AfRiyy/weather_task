8fa49e4178e03363c03d8d9f0fc1714c1c0626a5
AfRiyy <madaraszdavidd@gmail.com>
2022-05-01 20:19:49 +0200

    routes and middleware

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\PermissionController;
 use App\Models\Permission;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Gate;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,11 +20,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/permissions', [PermissionController::class, "index"]);
-Route::get('/permissions/create', [PermissionController::class, "create"]);
+Route::get('/permissions/show', [PermissionController::class, "show"]);
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('home');
 })->middleware(['auth'])->name('dashboard');
-
+Route::get('logout', [AuthenticatedSessionController::class, 'destroy']);
 require __DIR__ . '/auth.php';
+
+Route::middleware('auth')->group(function () {
+    Route::get('/posts', [PostController::class, "index"])->name('posts');
+    Route::put('/posts/update', [PostController::class, "update"]);
+    Route::get('/posts/delete/{id}', [PostController::class, "destroy"]);
+    Route::put('/permissions/edit/{id}', [PermissionController::class, "update"]);
+    Route::get('/permissions', [PermissionController::class, "index"])->name('permissions');
+    Route::get('/permissions/create', [PermissionController::class, "create"]);
+    Route::post('/posts/create', [PostController::class, "create"]);
+});
