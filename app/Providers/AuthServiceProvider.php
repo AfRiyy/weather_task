6c9f9eeafe9e29907ebfc8593ad04f2c494e3a20
AfRiyy <madaraszdavidd@gmail.com>
2022-05-01 20:20:52 +0200

    Permission gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+use App\Models\Post;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -25,6 +27,33 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('set-permission', function(){
+            $id = auth()->user()->id;
+            $permission = DB::table('userpermissions')->select('permission_id')->where('permission_id','=',1)->where('user_id',$id)->first();
+            if($permission != null){
+                return true;
+            }
+        });
+        Gate::define('create-post', function(){
+            $id = auth()->user()->id;
+            $permission = DB::table('userpermissions')->select('permission_id')->where('permission_id','=',2)->where('user_id',$id)->first();
+            if($permission != null){
+                return true;
+            }
+        });
+        Gate::define('edit-post', function(){
+            $id = auth()->user()->id;
+            $permission = DB::table('userpermissions')->select('permission_id')->where('permission_id','=',3)->where('user_id',$id)->first();
+            if($permission != null){
+                return true;
+            }
+        });
+        Gate::define('delete-post', function(){
+            $id = auth()->user()->id;
+            $permission = DB::table('userpermissions')->select('permission_id')->where('permission_id','=',4)->where('user_id',$id)->first();
+            if($permission != null){
+                return true;
+            }
+        });
     }
 }
